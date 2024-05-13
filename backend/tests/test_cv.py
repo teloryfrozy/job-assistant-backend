9ec55f729059f2f1b05b9da33f4a348f1916347c
teloryfrozy <raptor.frozy@gmail.com>
2024-05-13 13:02:12 +0300

    Add CV infos extractor function

--- backend/tests/test_cv.py
+++ backend/tests/test_cv.py
@@ -1,20 +1,28 @@
-"""
-TODO: look several CV and add an optimization Layer
-"""
-
-
 import PyPDF2
 
-def extract_keywords(pdf_file_path):
+
+def extract_keywords(pdf_file_path: str) -> str:
+    """
+    Extracts keywords from a PDF file.
+
+    Args:
+        pdf_file_path (str): The file path of the PDF.
+
+    Returns:
+        str: A string containing extracted keywords.
+    """
     text_keywords = ""
-    with open(pdf_file_path, 'rb') as file:
+    with open(pdf_file_path, "rb") as file:
         pdf_reader = PyPDF2.PdfReader(file)
         for page in pdf_reader.pages:
             text = page.extract_text()
             text_keywords += text
     return text_keywords.strip()
 
-pdf_file_path = 'backend/tests/resume_Augustin_ROLET.pdf'
-pdf_file_path = "backend/tests/CV Augustin ROLET - 2022.pdf"
+
+"""
+# Example usage
+pdf_file_path = "backend/tests/resume_Augustin_ROLET.pdf"
 text_keywords = extract_keywords(pdf_file_path)
 print(text_keywords)
+"""
